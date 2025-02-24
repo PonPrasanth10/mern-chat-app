1f996c21f446c387268ae550cabf957ad75ed21b
PonPrasanth10 <ponprasanthr@gmail.com>
2025-02-25 00:13:40 +0530

    comp

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 if(process.env.NODE_ENV==="production"){
-    app.use(express.static(patg.join(__dirname , "../frontend/dist")))
+    app.use(express.static(path.join(__dirname , "../frontend/dist")))
 
     app.get("*", (req,res) => {
         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
