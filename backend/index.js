ad6c9949d7d11a493449c2b6930b22efcfb5114e
PonPrasanth10 <ponprasanthr@gmail.com>
2025-02-25 13:59:32 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -8,28 +8,19 @@ import messageRoute from "./routes/message.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors"
 import { app,server } from "./lib/socket.js";
-import path from "path";
+
 
 
 dotenv.config()
 
 const PORT=process.env.PORT;
-const __dirname = path.resolve();
 
 // Use express.json() with a custom limit
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname , "../frontend/dist")))
-
-    app.get("*", (req,res) => {
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-    })
-}
-
 app.use(cors({
-    origin:["http://localhost:5173","https://mern-chat-app-4m3r.onrender.com"],
+    origin:["http://localhost:5173"],
     credentials:true,
 }))
 app.use(cookieParser())
